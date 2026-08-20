2b48702bcc19300f7ff6f0d5f72e322ac7b39695
Meohuso <310178674+Meohuso@users.noreply.github.com>
2026-08-20 13:25:12 +0200

    feat(cis): implement CIS 1.1.5.2 (/var/log nodev)

---
+++ modules/cis/1/1/5/2/module.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Rocky Linux CIS Hardening Framework
+#
+# CIS 1.1.5.2 - Ensure nodev option is set on /var/log partition.
+#
+# SPDX-License-Identifier: MIT
+#
+
+readonly RLCH_CIS_1_1_5_2_MOUNT_TARGET="/var/log"
+readonly RLCH_CIS_1_1_5_2_MOUNT_OPTION="nodev"
+
+RLCH_CIS_1_1_5_2_FSTAB="${RLCH_CIS_1_1_5_2_FSTAB:-${RLCH_MOUNT_FSTAB:-/etc/fstab}}"
+
+check() {
+    local result
+
+    mount_check_option         "${RLCH_CIS_1_1_5_2_MOUNT_TARGET}"         "${RLCH_CIS_1_1_5_2_MOUNT_OPTION}"         "${RLCH_CIS_1_1_5_2_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
+
+apply() {
+    local result
+
+    mount_apply_option         "${RLCH_CIS_1_1_5_2_MOUNT_TARGET}"         "${RLCH_CIS_1_1_5_2_MOUNT_OPTION}"         "${RLCH_CIS_1_1_5_2_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
+
+validate() {
+    check
+}
+
+rollback() {
+    local result
+
+    mount_rollback         "${RLCH_CIS_1_1_5_2_MOUNT_TARGET}"         "${RLCH_CIS_1_1_5_2_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
