e9a70afcf58d7ad23546558292db57b07a2b2aca
Meohuso <310178674+Meohuso@users.noreply.github.com>
2026-08-20 12:11:02 +0200

    feat(cis): implement CIS 1.1.3.3 (/var nosuid)

---
+++ modules/cis/1/1/3/3/module.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Rocky Linux CIS Hardening Framework
+#
+# CIS 1.1.3.3 - Ensure nosuid option is set on /var partition.
+#
+# SPDX-License-Identifier: MIT
+#
+
+readonly RLCH_CIS_1_1_3_3_MOUNT_TARGET="/var"
+readonly RLCH_CIS_1_1_3_3_MOUNT_OPTION="nosuid"
+
+RLCH_CIS_1_1_3_3_FSTAB="${RLCH_CIS_1_1_3_3_FSTAB:-${RLCH_MOUNT_FSTAB:-/etc/fstab}}"
+
+check() {
+    local result
+
+    mount_check_option         "${RLCH_CIS_1_1_3_3_MOUNT_TARGET}"         "${RLCH_CIS_1_1_3_3_MOUNT_OPTION}"         "${RLCH_CIS_1_1_3_3_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
+
+apply() {
+    local result
+
+    mount_apply_option         "${RLCH_CIS_1_1_3_3_MOUNT_TARGET}"         "${RLCH_CIS_1_1_3_3_MOUNT_OPTION}"         "${RLCH_CIS_1_1_3_3_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
+
+validate() {
+    check
+}
+
+rollback() {
+    local result
+
+    mount_rollback         "${RLCH_CIS_1_1_3_3_MOUNT_TARGET}"         "${RLCH_CIS_1_1_3_3_FSTAB}"
+    result=$?
+
+    if [[ "${result}" -eq "${RLCH_MODULE_RESULT_COMPLIANT}" ]]; then
+        return "${RLCH_MODULE_RESULT_SUCCESS}"
+    fi
+
+    return "${result}"
+}
